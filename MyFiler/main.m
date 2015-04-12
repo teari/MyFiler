4d681f978af21eecdf5c37d83534bd6551215771
早川拓人 <takuto-macbookair@hayakawatakuto-no-MacBook-Air.local>
2015-04-12 22:15:27 +0900

    Initial Commit

---
+++ MyFiler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyFiler
+//
+//  Created by 早川拓人 on 2015/04/12.
+//  Copyright (c) 2015年 takuto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
